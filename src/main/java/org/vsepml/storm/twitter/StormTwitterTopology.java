d074e683c25ce2c05146fea7565eb5e2f987d52c
nicolasferry <nicolas.ferry@sintef.no>
2015-01-15 19:48:24 +0100

    credentials in property file

--- src/main/java/org/vsepml/storm/twitter/StormTwitterTopology.java
+++ src/main/java/org/vsepml/storm/twitter/StormTwitterTopology.java
@@ -22,7 +22,13 @@ import backtype.storm.generated.AlreadyAliveException;
 import backtype.storm.generated.InvalidTopologyException;
 import backtype.storm.topology.TopologyBuilder;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by ferrynico on 10/01/15.
@@ -30,6 +36,7 @@ import java.util.ArrayList;
 public class StormTwitterTopology {
 
     static final String TOPOLOGY_NAME = "storm-twitter";
+    private static final Logger journal = Logger.getLogger(StormTwitterTopology.class.getName());
 
     public static void main(String[] args) throws AlreadyAliveException, InvalidTopologyException {
         Config config = new Config();
@@ -37,16 +44,38 @@ public class StormTwitterTopology {
 
         TopologyBuilder b = new TopologyBuilder();
 
-        b.setSpout("TwitterStreamSpout", new StormTwitterStreamSpout(args[0],args[1],args[2],args[3]),1);
-        b.setBolt("Splitter", new StormTwitterHashtagSplitter(),2)
-                .shuffleGrouping("TwitterStreamSpout");
-        ArrayList<String> identifiers= new ArrayList<String>();
-        identifiers.add("oslo");
-        b.setBolt("Identifier", new StormTwitterHashTagIdentifier(identifiers),1)
-                .shuffleGrouping("Splitter");
+        Properties prop = new Properties();
+        InputStream input = null;
+        try {
 
-        StormSubmitter.submitTopology(TOPOLOGY_NAME, config, b.createTopology());
+            input = new FileInputStream("twitter.properties");
 
+            // load a properties file
+            prop.load(input);
+
+            b.setSpout("TwitterStreamSpout", new StormTwitterStreamSpout(prop.getProperty("accessToken"),
+                    prop.getProperty("accessTokenSecret"),prop.getProperty("consumerKey"),prop.getProperty("consumerSecret")),1);
+            b.setBolt("Splitter", new StormTwitterHashtagSplitter(),2)
+                    .shuffleGrouping("TwitterStreamSpout");
+            ArrayList<String> identifiers= new ArrayList<String>();
+            identifiers.add("oslo");
+            b.setBolt("Identifier", new StormTwitterHashTagIdentifier(identifiers),1)
+                    .shuffleGrouping("Splitter");
+
+            StormSubmitter.submitTopology(TOPOLOGY_NAME, config, b.createTopology());
+
+
+        } catch (IOException ex) {
+            journal.log(Level.INFO, ">> monitoring.properties not found status monitor not in use");
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
     }
 
